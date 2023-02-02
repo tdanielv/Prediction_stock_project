7108ca1fdebb8c66b5f214b7f5ad1eefdfcfa5ce
Daniil Tkalich <103593586+tdanielv@users.noreply.github.com>
2023-02-02 15:40:10 +0400

    second step

--- telebot.py
+++ telebot.py
@@ -1,11 +1,31 @@
+import time
+
 import requests
-
-api_url = 'http://api.open-notify.org/iss-now.json'
-
-response = requests.get(api_url)
+import json
+import pprint
 
 
-if response.status_code == 200:
-    print(response.text)
-else:
-    print(response.status_code)
+BOT_TOKEN  = '5321476850:AAGgKK501MtxMe9tUVLtEw2VxS0LKiZlZ2g'
+METHOD_NAME = 'sendMessage'
+CHAT_ID = '580379590'
+TEXT = 'Hello'
+API_URL = f'https://api.telegram.org/bot'
+MAX_COUNTER = 100
+OFFSET = -2
+COUNTER = 0
+response = requests.get(API_URL)
+
+while COUNTER < MAX_COUNTER:
+    print('attempt = ', COUNTER)
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={OFFSET+1}').json()
+    if updates['result']:
+        for result in updates['result']:
+            OFFSET = result['update_id']
+            chat_id = result['message']['from']['id']
+            name =  result['message']['from']['first_name']
+            text = f'Hello, {name}'
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
+
+    time.sleep(1)
+    COUNTER += 1
